2393bd6559977edd2ecc5029f554d4c0d1226e29
Marcos653 <marcos.costas@xbrain.com.br>
2023-06-18 10:32:27 -0300

    feat: implement movie html

--- src/main/java/br/com/alura/dayscode/controller/MovieController.java
+++ src/main/java/br/com/alura/dayscode/controller/MovieController.java
@@ -4,8 +4,9 @@ import br.com.alura.dayscode.model.Movie;
 import br.com.alura.dayscode.service.MovieService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -19,8 +20,13 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @RequestMapping("/top-movies")
-    public List<Movie> getTopMovies() throws JsonProcessingException {
-        return movieService.getTopMovies();
+    @GetMapping("/top-movies")
+    public ModelAndView getTopMovies() throws JsonProcessingException {
+        List<Movie> movies = movieService.getTopMovies();
+        ModelAndView modelAndView = new ModelAndView("Movies");
+
+        modelAndView.addObject("movies", movies);
+
+        return modelAndView;
     }
 }
